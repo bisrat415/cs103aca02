069451a3ad2619b2798152ca612998825ec35579
talspec2 <99153309+talspec2@users.noreply.github.com>
2023-05-05 14:33:05 -0400

    made chatgpt work, but hit quota

--- firstapp/routes/talRoute.js
+++ firstapp/routes/talRoute.js
@@ -5,6 +5,10 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+require('dotenv').config();
+const GPT = require('openai-api');
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const gpt = new GPT(OPENAI_API_KEY);
 const Story = require('../models/story');
 const User = require('../models/User');
 
@@ -17,13 +21,25 @@ isLoggedIn = (req,res,next) => {
     }
 }
 
+async function generateStory(topic) {
+    const prompt = `Once upon a time, there was a ${topic}. The ${topic} had an adventure.`;
+    const response = await gpt.complete({
+      prompt,
+      maxTokens: 100,
+      n: 1,
+      stop: '\n',
+    });
+    return response.data.choices[0].text.trim();
+}
+
+
 router.get('/tal', isLoggedIn, (req, res) => {
   res.render('tal');
 });
 
 router.post('/story', isLoggedIn, async (req, res) => {
   const topic = req.body.topic;
-  const story = await chatgpt.generateStory(topic);
+  const story = await generateStory(topic);
 
   // Save the generated story to the database
   const newStory = new Story({
